11da3e9c98bd012ff8b7538cbbb31cf6b0752476
lucassilva3g <lucassantos.santos90@hotmai.com>
2024-01-09 16:01:18 -0300

    desafio poo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone smartphone2 = new(numero: "987654321", modelo: "iphone1", imei: "def5678", memoria: 2048);
+Nokia smartphone1 = new(numero: "123456789", modelo: "nokia1", imei: "abc1234", memoria: 2048);
+
+Console.WriteLine("Smartphone Iphone:");
+smartphone2.Ligar();
+smartphone2.InstalarAplicativo(nomeApp: "Facebook");
+smartphone2.ReceberLigacao();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+smartphone1.Ligar();
+smartphone1.InstalarAplicativo(nomeApp: "Whatsapp");
+smartphone1.ReceberLigacao();
